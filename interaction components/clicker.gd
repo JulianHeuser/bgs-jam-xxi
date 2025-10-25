95ec912ac31d976462968af611a3b0f583d86b18
AvatarKeing <80914057+AvatarKeing@users.noreply.github.com>
2025-10-25 12:52:06 -0400

    Dialogue fixes

--- interaction components/clicker.gd
+++ interaction components/clicker.gd
@@ -1,10 +1,18 @@
 extends Sprite2D
 
 @export var dialogue : DialogueResource
+var interactable = true
+
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(
+		func(_resource: DialogueResource): interactable = false
+	)
+	DialogueManager.dialogue_ended.connect(
+		func(_resource: DialogueResource): interactable = true
+	)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and interactable:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if is_pixel_opaque(get_local_mouse_position()):
-				print("clicked")
 				DialogueManager.show_dialogue_balloon(dialogue,"start")
