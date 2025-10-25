b10dafff53921a0f570146450eab59e2e3605d60
AvatarKeing <80914057+AvatarKeing@users.noreply.github.com>
2025-10-25 11:12:27 -0400

    11:11 Click action and dialogue

---
+++ global/global.gd
@@ -0,0 +1,13 @@
+extends Node
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/bedroom.tscn")
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_direction_pressed():
+	get_tree().change_scene_to_file("res://scenes/directions.tscn")
