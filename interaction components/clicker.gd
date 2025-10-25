601106f789cf267463aaa8220662b86785c7a539
AvatarKeing <80914057+AvatarKeing@users.noreply.github.com>
2025-10-25 13:59:49 -0400

    item names

--- interaction components/clicker.gd
+++ interaction components/clicker.gd
@@ -1,7 +1,10 @@
-extends Sprite2D
+class_name Clicker extends Sprite2D
 
 @export var dialogue : DialogueResource
+@export var item_name : String
 var interactable = true
+
+static var selected_items : Array[String]
 
 func _ready() -> void:
 	DialogueManager.dialogue_started.connect(
@@ -15,4 +18,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and interactable:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if is_pixel_opaque(get_local_mouse_position()):
-				DialogueManager.show_dialogue_balloon(dialogue,"start")
+				DialogueManager.show_dialogue_balloon(dialogue,"start", [self])
+
+func add_item_to_clues() -> void:
+	print("ADDED THE THING!")
