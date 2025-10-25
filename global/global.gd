fb2e298ca20664d6c71b45984e952427ded3101f
AvatarKeing <80914057+AvatarKeing@users.noreply.github.com>
2025-10-25 16:31:45 -0400

    menu changes

--- global/global.gd
+++ global/global.gd
@@ -15,7 +15,3 @@ func _on_direction_pressed():
 
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
-
-
-func _on_directions_pressed() -> void:
-	pass # Replace with function body.
