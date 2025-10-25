e166b6a4cd93edd97bbfdd1741286a6873a34c85
AvatarKeing <80914057+AvatarKeing@users.noreply.github.com>
2025-10-25 11:34:22 -0400

    11:33 more dialogue clicking

--- interaction components/clicker.gd
+++ interaction components/clicker.gd
@@ -5,3 +5,4 @@ func _input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if is_pixel_opaque(get_local_mouse_position()):
 				print("clicked")
+				DialogueManager.show_dialogue_balloon(load("res://writing'.dialogue"),"start")
